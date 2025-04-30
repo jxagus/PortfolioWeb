b5c546d8003f09a60b395d08e0e82f443069c7b8
agust <agustin198ar@gmail.com>
2025-04-30 07:13:21 -0300

    Agregar archivos de proyecto.

---
+++ PortfolioWeb/script.js
@@ -0,0 +1,13 @@
+﻿const secciones = document.querySelectorAll('section');
+
+const mostrarSeccion = () => {
+    secciones.forEach(sec => {
+        const top = sec.getBoundingClientRect().top;
+        if (top < window.innerHeight - 100) {
+            sec.classList.add('visible');
+        }
+    });
+};
+
+window.addEventListener('scroll', mostrarSeccion);
+window.addEventListener('load', mostrarSeccion);
